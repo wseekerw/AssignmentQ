725aadc6b976ef014f5024d23a5f0203259e289a
L <tasic.lazar@gmail.com>
2022-12-05 20:32:49 +0100

    Refactor user data

--- src/shared/reference-name/reference-name.pipe.ts
+++ src/shared/reference-name/reference-name.pipe.ts
@@ -1,22 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { User, TransformedUser } from '../../app/posts/posts.model';
+import { User } from '../../app/posts/posts.model';
 
 @Pipe({
   name: 'referenceName',
 })
 export class ReferenceNamePipe implements PipeTransform {
-  transform(id: number | null, data: User[] | null): TransformedUser {
-    if (!id || !data || !data.length) return {};
-    const obj = data!.find((d: { id: number }) => d.id == id)
-
-    return {
-      name: obj?.name,
-      username: obj?.username,
-      email: obj?.email,
-      address: obj?.address,
-      phone: obj?.phone,
-      website: obj?.website,
-      company: obj?.company,
-    };
+  transform(id: number | null, data: User[] | null): string {
+    if (!id || !data || !data.length) return '';
+    return data!.find((d: { id: number }) => d.id == id)!.name
   }
 }
